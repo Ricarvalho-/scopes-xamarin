1c01d06f02ab21b15fbf02cd77d6c372fb354ef9
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2020-10-21 02:41:49 -0300

    Services implementation

---
+++ Scopes/Service/GoalFirebaseService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Scopes.Model;
+
+namespace Scopes.Service
+{
+    public class GoalFirebaseService
+    {
+        private readonly FirebaseClient Database;
+
+        public GoalFirebaseService(FirebaseClient firebaseClient)
+        {
+            Database = firebaseClient;
+        }
+
+        public async Task<List<FirebaseObject<Goal>>> AllGoalsOfScope(FirebaseObject<Scope> scope)
+        {
+            return (await Database.Child("goals").Child(scope.Key).OnceAsync<Goal>()).ToList();
+        }
+
+        public async Task<FirebaseObject<Goal>> CreateGoal(FirebaseObject<Scope> scope, string title)
+        {
+            return await Database.Child("goals").Child(scope.Key).PostAsync(new Goal
+            {
+                Title = title,
+                Progress = 0
+            });
+        }
+
+        public async Task UpdateGoal(FirebaseObject<Scope> scope, FirebaseObject<Goal> goal, string title = null, int? progress = null)
+        {
+            await Database.Child("goals").Child(scope.Key).Child(goal.Key).PutAsync(new Goal
+            {
+                Title = title ?? goal.Object.Title,
+                Progress = progress ?? goal.Object.Progress
+            });
+        }
+
+        public async Task DeleteGoal(FirebaseObject<Scope> scope, FirebaseObject<Goal> goal)
+        {
+            await Database.Child("goals").Child(scope.Key).Child(goal.Key).DeleteAsync();
+        }
+    }
+}
---
+++ Scopes/Service/ScopeFirebaseService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Scopes.Model;
+
+namespace Scopes.Service
+{
+    public class ScopeFirebaseService
+    {
+        private readonly FirebaseClient Database;
+
+        public ScopeFirebaseService(FirebaseClient firebaseClient)
+        {
+            Database = firebaseClient;
+        }
+
+        public async Task<List<FirebaseObject<Scope>>> AllScopes()
+        {
+            return (await Database.Child("scopes").OnceAsync<Scope>()).ToList();
+        }
+
+        public async Task<FirebaseObject<Scope>> CreateScope(string title)
+        {
+            return await Database.Child("scopes").PostAsync(new Scope
+            {
+                Title = title
+            });
+        }
+
+        public async Task UpdateScope(FirebaseObject<Scope> scope, string title)
+        {
+            await Database.Child("scopes").Child(scope.Key).PutAsync(new Scope
+            {
+                Title = title
+            });
+        }
+
+        public async Task DeleteScope(FirebaseObject<Scope> scope)
+        {
+            await Task.WhenAll(
+                Database.Child("scopes").Child(scope.Key).DeleteAsync(),
+                Database.Child("goals").Child(scope.Key).DeleteAsync()
+                );
+        }
+    }
+}
